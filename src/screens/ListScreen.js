f7c62c6515da795c78d9556e7cd2b3346854d421
Alec Wells <alec.wells47@gmail.com>
2020-03-03 11:27:59 -0700

    Add margin to stylesheet

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -15,15 +15,20 @@ const ListScreen = () => {
   ];
   return (
     <FlatList
+    // horizontal
     keyExtractor={(friend) => friend.name}
       data={friends}
       renderItem={({ item }) => {
-        return <Text>{item.name}</Text>
+        return <Text style={styles.textStyle}>{item.name}</Text>
       }}
       />
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50
+  }
+});
 
 export default ListScreen;
