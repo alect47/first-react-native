d5e1191eb7749c005e5478d532fd37a589ee8f2c
Alec Wells <alec.wells47@gmail.com>
2020-03-20 19:47:57 -0600

    Add color setters for blue and green

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -7,16 +7,22 @@ const SquareScreen = () => {
   const[green, setGreen] = useState(0);
   const[blue, setBlue] = useState(0);
 
-  console.log(red);
-  
   return <View>
     <ColorCounter
       onIncrease={() => setRed(red + 1)}
       onDecrease={() => setRed(red - 1)}
       color="Red"
       />
-    <ColorCounter color="Blue" />
-    <ColorCounter color="Green" />
+    <ColorCounter
+      onIncrease={() => setBlue(blue + 1)}
+      onDecrease={() => setBlue(blue - 1)}
+      color="Blue"
+    />
+    <ColorCounter
+      onIncrease={() => setGreen(green + 1)}
+      onDecrease={() => setGreen(green - 1)}
+      color="Green"
+    />
   </View>
 };
 
