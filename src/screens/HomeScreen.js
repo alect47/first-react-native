b3436199c8b2fa7b02eb06aa4dd0c2ce7ae7b910
Alec Wells <alec.wells47@gmail.com>
2020-03-20 19:25:48 -0600

    Add square to home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -26,6 +26,10 @@ const HomeScreen = ({navigation}) => {
         title="Go to Color"
         onPress={() => navigation.navigate('Color')}
       />
+      <Button
+        title="Go to Square Screen"
+        onPress={() => navigation.navigate('Square')}
+      />
     </View>
   );
 };
