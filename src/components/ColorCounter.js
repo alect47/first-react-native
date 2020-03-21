00e70895b4094a9d1446465307eaf1550afdc489
Alec Wells <alec.wells47@gmail.com>
2020-03-20 19:27:25 -0600

    Add color counter component

---
+++ src/components/ColorCounter.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text, StyleSheet} from 'react-native';
+
+const SquareScreen = () => {
+  return <Text>Square Screen</Text>
+};
+
+const style = StyleSheet.create({});
