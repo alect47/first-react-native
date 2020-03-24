27c48571d39bdca79eb827ae23c08ff5d51c6f7d
Alec Wells <alec.wells47@gmail.com>
2020-03-24 13:35:34 -0600

    Add reducer for changing color

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,10 +1,26 @@
-import React, { useState} from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet} from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
 const COLOR_INCREMENT = 15;
 
+const reducer = (state, action) => {
+  // state === {red: number, green: number, blue: number};
+  // action === { colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15}
+
+  switch (action.colorToChange) {
+    case 'red':
+      
+    case 'green':
+    case 'blue':
+    default:
+
+};
+
 const SquareScreen = () => {
+
+
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0});
 
   return <View>
     <ColorCounter
