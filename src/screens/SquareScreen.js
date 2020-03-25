c85dd1608030d8987290a0ea14d9e0f9bcff631c
Alec Wells <alec.wells47@gmail.com>
2020-03-25 17:30:22 -0600

    Change square screen component to align with convention

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -6,7 +6,7 @@ const COLOR_INCREMENT = 15;
 
 const reducer = (state, action) => {
   // state === {red: number, green: number, blue: number};
-  // action === { colorToChange: 'red' || 'green' || 'blue', amount: 15 || -15}
+  // action === { type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15}
 
   switch (action.colorToChange) {
     case 'red':
@@ -32,18 +32,18 @@ const SquareScreen = () => {
 
   return <View>
     <ColorCounter
-      onIncrease={() => dispatch({colorToChange: 'red', amount: COLOR_INCREMENT})}
-      onDecrease={() => dispatch({colorToChange: 'red', amount: -1 *  COLOR_INCREMENT})}
+      onIncrease={() => dispatch({type: 'red', payload: COLOR_INCREMENT})}
+      onDecrease={() => dispatch({type: 'red', payload: -1 *  COLOR_INCREMENT})}
       color="Red"
       />
     <ColorCounter
-      onIncrease={() => dispatch({colorToChange: 'blue', amount: COLOR_INCREMENT})}
-      onDecrease={() => dispatch({colorToChange: 'blue', amount: -1 * COLOR_INCREMENT})}
+      onIncrease={() => dispatch({type: 'blue', payload: COLOR_INCREMENT})}
+      onDecrease={() => dispatch({type: 'blue', payload: -1 * COLOR_INCREMENT})}
       color="Blue"
     />
     <ColorCounter
-      onIncrease={() => dispatch({colorToChange: 'green', amount: COLOR_INCREMENT})}
-      onDecrease={() => dispatch({colorToChange: 'green', amount: -1 * COLOR_INCREMENT})}
+      onIncrease={() => dispatch({type: 'green', payload: COLOR_INCREMENT})}
+      onDecrease={() => dispatch({type: 'green', payload: -1 * COLOR_INCREMENT})}
       color="Green"
     />
     <View style={{
