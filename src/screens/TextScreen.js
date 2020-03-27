ec7efda6974664bfeaddb9058323a025991581b4
Alec Wells <alec.wells47@gmail.com>
2020-03-27 13:58:19 -0600

    Add useState and callback function to TextScreen

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
 
+// State in React is used to track any variable that is expected to change over time
+// We never want parent element(TextScreen) to inspect element/property of child(TextInput)
+
 const TextScreen = () => {
+  const [name, setName] = useState('');
+
   return (
     <View>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
+        value={name}
+        onChangeText={(newText) => setName(newText) }
       />
     </View>
   );
